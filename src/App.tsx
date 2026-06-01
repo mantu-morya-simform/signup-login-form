c92958f742614664a8cb89e9ea357e2886d94b3c
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-06-01 14:45:28 +0530

    chore: login-signup project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginSignup from "./component/LoginSignup";
+
+function App() {
+  return (
+    <>
+      <LoginSignup />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/LoginSignup.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const LoginSignup = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return <div>LoginSignup</div>;
+};
+
+export default LoginSignup;
